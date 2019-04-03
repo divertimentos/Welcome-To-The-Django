eba87340a0dc84545f633269b2e16aac9fd134dc
guiemi <guiemi.teixeira@gmail.com>
2019-04-03 16:31:11 -0300

    Modifica o import para testar o deply

--- eventex/urls.py
+++ eventex/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-import eventex.core.views
+# import eventex.core.views
+from eventex.core.views import home
 
 urlpatterns = [
     path('', eventex.core.views.home),
